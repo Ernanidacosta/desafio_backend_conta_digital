452503e474043bf32d26774fa7e3b176bd766d5e
Ernani <ernani.costa@vai.com.vc>
2023-05-24 10:54:18 -0300

    Add route root

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ app = FastAPI(title='API using FastAPI and SQL Alchemy')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
+@app.get("/")
+async def root():
+    return {"message": "IT'S WORK!!"}
+
+
 if __name__ == '__main__':
     import uvicorn
 
     uvicorn.run(
-        'main:app', host='0.0.0.0', port=8000, log_level='info', reload=True
+        'main:app', host='0.0.0.0', port=9000, log_level='info', reload=True
     )
