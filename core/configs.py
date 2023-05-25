e7b51f048dfbd4177aa89944dd8d773b82751740
Ernani <ernani.costa@vai.com.vc>
2023-05-25 08:02:16 -0300

    testing

--- core/configs.py
+++ core/configs.py
@@ -8,7 +8,9 @@ class Settings(BaseSettings):
     """
 
     API_V1_STR: str = '/api/v1'
-    BD_URL: str = 'postgresql+asyncpg://admin:admin@localhost:5432/digital_account_database'
+    # BD_URL: str = 'postgresql+asyncpg://admin:admin@localhost:5432/digital_account_database'
+    # BD_URL: str = 'postgresql+asyncpg://admin:F0LX8MapIloxCQ5c5sV92do88Lnt45pe@dpg-chn84iu4dad1d57c7kjg-a.oregon-postgres.render.com/digital_account_database'
+    BD_URL: str = 'sqlite+aiosqlite:///digital_account_database.db'
     DBBaseModel = declarative_base()
 
     class Config:
