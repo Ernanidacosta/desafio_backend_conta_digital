1dac18066185b276f96f7c0a6c534691bc7da62b
Ernani <contato@ernani.dev>
2023-05-26 17:23:44 -0300

    change port

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     import uvicorn
 
     uvicorn.run(
-        'main:app', host='0.0.0.0', port=9000, log_level='info', reload=True
+        'main:app', host='0.0.0.0', port=8000, log_level='info', reload=True
     )
