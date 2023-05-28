0b26d6baeb6dae00d0fb0cbd1ee05243b7fc1c88
Ernani <contato@ernani.dev>
2023-05-28 16:26:22 -0300

    Install python_decouple for get envs

--- core/configs.py
+++ core/configs.py
@@ -1,4 +1,4 @@
-import os
+from decouple import config
 from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
     """
 
     API_V1_STR: str = '/api/v1'
-    # BD_URL: str = 'postgresql+asyncpg://admin:admin@localhost:5432/digital_account_database'
-    BD_URL: str = os.getenv('DATABASE_URL')
+    BD_URL: str = config('LOCAL_DATABASE_URL')
+    # BD_URL: str = config('REMOTE_DATABASE_URL')
     DBBaseModel = declarative_base()
 
     class Config:
