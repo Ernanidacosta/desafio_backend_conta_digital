d014ecd6d660f1ab043ef9175c216572023bae58
Ernani <contato@ernani.dev>
2023-05-28 01:01:48 -0300

    Hide sensive data

--- core/configs.py
+++ core/configs.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,7 +10,7 @@ class Settings(BaseSettings):
 
     API_V1_STR: str = '/api/v1'
     # BD_URL: str = 'postgresql+asyncpg://admin:admin@localhost:5432/digital_account_database'
-    BD_URL: str = 'postgresql+asyncpg://admin:F0LX8MapIloxCQ5c5sV92do88Lnt45pe@dpg-chn84iu4dad1d57c7kjg-a.oregon-postgres.render.com/digital_account_database'
+    BD_URL: str = os.getenv('DATABASE_URL')
     DBBaseModel = declarative_base()
 
     class Config:
